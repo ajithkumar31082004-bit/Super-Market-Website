42fb9bd145a1ec4d2c64328339ff8670691e7f60
Ajith Kumar <ajithkumar31082004@gmail.com>
2026-08-13 19:44:19 +0530

    feat: provision EKS cluster with managed node group, essential add-ons, and AWS Load Balancer Controller

--- supermarket-eks/eks.tf
+++ supermarket-eks/eks.tf
@@ -41,6 +41,7 @@ resource "aws_eks_node_group" "main" {
   node_group_name = "${var.project_name}-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+  ami_type        = "AL2023_x86_64_STANDARD"
   instance_types  = var.eks_node_instance_types
   disk_size       = var.eks_node_disk_size
 
